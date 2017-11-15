5d3a7bdf41e6246b79819c0fc9b5cef09cf1c257
craftwar <coo.ky@hotmail.com>
2017-11-15 16:31:19 +0800

    Add NightBot support and improve performance

--- js/global.js
+++ js/global.js
@@ -1,79 +1,21 @@
-var map = (function() {
-    var _maxLength = 200;
-
-    var _map = Object.create(null);
-    var _keys = [];
-
-    function map() {
-        this.len = function() {
-            return _keys.length;
-        };
-
-        this.setMaxLength = function(len) {
-            _maxLength = len;
-        };
-
-        this.getMaxLength = function() {
-            return _maxLength;
-        };
-
-        this.insert = function(key, value) {
-            if (this.len.apply() == this.getMaxLength.apply() &&
-                typeof _map[key] == "undefined") {
-                var id = _keys.shift();
-                delete _map[id];
-            }
-
-            _map[key] = value;
-            if (!this.contains(key)) {
-                _keys.push(key);
-            }
-        };
-
-        this.value = function(key) {
-            return _map[key];
-        };
-
-        this.contains = function(key) {
-            return typeof _map[key] != "undefined";
-        };
-
-        this.remove = function(key) {
-            if (this.contains(key)) {
-                delete _map[key];
-            }
-        };
-
-        this.clear = function() {
-            _map = Object.create(null);
-            _keys = [];
-        }
-    }
-
-    return map;
-
-})();
-
+var tabIds = new Map();
 function removeURLParameters(url, parameters) {
-    parameters.forEach(function(parameter) {
-        var urlparts = url.split('?');
-        if (urlparts.length >= 2) {
-            var prefix = encodeURIComponent(parameter) + '=';
-            var pars = urlparts[1].split(/[&;]/g);
+    let urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+        let pars = urlparts[1].split(/[&;]/g);
 
-            for (var i = pars.length; i-- > 0;) {
-                if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+        for (let i = 0; i < pars.length; ++i) {
+            parameters.forEach(function(parameter){
+                if (pars[i].startsWith(parameter)) {
                     pars.splice(i, 1);
+                    return;
                 }
-            }
-
-            url = urlparts[0] + '?' + pars.join('&');
+            });
         }
-    });
+        url = urlparts[0] + '?' + pars.join('&');
+    }
     return url;
 }
-
-var tabIds = new map();
 
 function reloadTab() {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
@@ -86,17 +28,17 @@ function reloadTab() {
 }
 
 function sendMessage(tabId) {
-    if (tabIds.contains(tabId)) {
-        chrome.tabs.sendMessage(tabId, {url: tabIds.value(tabId)});
+    if (tabIds.has(tabId)) {
+        chrome.tabs.sendMessage(tabId, {url: tabIds.get(tabId)});
     }
 }
 
 function processRequest(details) {
     if (details.url.indexOf('mime=audio') !== -1) {
-        var parametersToBeRemoved = ['range', 'rn', 'rbuf'];
+        var parametersToBeRemoved = ['range=', 'rn=', 'rbuf='];
         var audioURL = removeURLParameters(details.url, parametersToBeRemoved);
-        if (tabIds.value(details.tabId) != audioURL) {
-            tabIds.insert(details.tabId, audioURL);
+        if (tabIds.get(details.tabId) != audioURL) {
+            tabIds.set(details.tabId, audioURL);
             chrome.tabs.sendMessage(details.tabId, {url: audioURL});
         }
     }
@@ -123,9 +65,14 @@ function disableExtension() {
             38 : "img/disabled_icon38.png",
         }
     });
+/*    
+    tabIds.forEach(function(value, key, map){
+        chrome.tabs.sendMessage(key, {url: ""});
+    })
+*/    
+    tabIds.clear();
     chrome.tabs.onUpdated.removeListener(sendMessage);
     chrome.webRequest.onBeforeRequest.removeListener(processRequest);
-    tabIds.clear();
 }
 
 function saveSettings(disabled) {
--- js/youtube_audio.js
+++ js/youtube_audio.js
@@ -1,35 +1,25 @@
-function makeSetAudioURL(videoElement, url) {
-    function setAudioURL() {
-        if (videoElement.src  != url) {
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        let url = request.url;
+        let videoElement = document.getElementsByTagName('video')[0];
+        /*
+        let oldUrl;
+
+        if (oldUrl === undefined) {
+            oldUrl = videoElement.src;
+        }
+        if (url == "") {
+            videoElement.pause();
+            videoElement.src = oldUrl;
+            videoElement.currentTime = 0;
+            videoElement.play();
+        } else 
+            */
+            if (videoElement.src  != url) {
             videoElement.pause();
             videoElement.src = url;
             videoElement.currentTime = 0;
             videoElement.play();
-        }
-    }
-    setAudioURL();
-    return setAudioURL;
-}
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        var url = request.url;
-        var videoElements = document.getElementsByTagName('video');
-        var videoElement = videoElements[0];
-        videoElement.onloadeddata = makeSetAudioURL(videoElement, url);
-        if (document.getElementsByClassName('audio_only_div').length == 0) {
-            var extensionAlert = document.createElement('div');
-            extensionAlert.className = 'audio_only_div';
-
-            var alertText = document.createElement('p');
-            alertText.className = 'alert_text';
-            alertText.innerHTML = 'Youtube Audio Extension is running. It disables the video stream and uses only the audio stream' +
-                ' which saves battery life and bandwidth / data when you just want to listen to just songs. If you want to watch' +
-                ' video also, click on the extension icon and refresh your page.';
-
-            extensionAlert.appendChild(alertText);
-            var parent = videoElement.parentNode.parentNode;
-            parent.appendChild(extensionAlert);
         }
     }
 );
