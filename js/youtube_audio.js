0026aaee5096befe67ebeeb5e6f6e910be62bc2e
craftwar <craftwar@users.noreply.github.com>
2018-12-30 16:33:54 +0800

    Try to fix issue#3 Remove condition "!videoElement.src.includes('mime=audio')". It was intended to pick first matching audio stream. (not to change stream when newer audio stream of the same video is found.)

--- js/youtube_audio.js
+++ js/youtube_audio.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
         const url = request.url;
         const videoElement = document.getElementsByTagName('video')[0];
-        if ( !videoElement.src.includes('mime=audio') && videoElement.src != url) {
+        if (videoElement.src != url) {
             videoElement.src = url;
             videoElement.play();
         }
