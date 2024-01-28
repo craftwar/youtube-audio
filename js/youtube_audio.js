21cad42c5a0d4a2b155de3e6de5155461c3d476b
craftwar <craftwar@users.noreply.github.com>
2024-01-29 07:18:57 +0800

    Fix playlist autoplay (issue #13)

--- js/youtube_audio.js
+++ js/youtube_audio.js
@@ -9,8 +9,8 @@ chrome.runtime.onMessage.addListener(
 			let currentTime = videoElement.currentTime;
 			videoElement.src = url;
 			videoElement.currentTime = currentTime;
-			if (!videoElement.paused)
-				videoElement.play();
+			// if (!videoElement.paused)
+			videoElement.play();
 		}
 	}
 );
